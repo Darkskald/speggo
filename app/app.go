bafed4fba33c82720596353b4316f00499dc4efb
flo <floriandavid.lange@web.de>
2021-06-24 22:10:03 +0200

    new gui

--- app/app.go
+++ app/app.go
@@ -61,7 +61,7 @@ func (a App) IndexHandler(c *gin.Context) {
 	c.HTML(
 		// Set the HTTP status to 200 (OK)
 		http.StatusOK,
-		// Use the index.html template
+		// Use the old_index.html template
 		"index.html",
 		// Pass the data that the page uses (in this case, 'title')
 		gin.H{
--- templates/assets/js/custom_scripts.js
+++ templates/assets/js/custom_scripts.js
@@ -3,7 +3,7 @@
     // the DOM will be available
     console.log("Welcome to custom scripting in speggo!");
 
-    const form = document.getElementById('searchForm');
+    /*const form = document.getElementById('searchForm');
 
 
     form.addEventListener('submit', function (){
@@ -19,7 +19,7 @@
 
 
 
-    });
+    });*/
 
     fetch('/sfg/3')
         .then(res => res.json())
@@ -29,44 +29,57 @@
         }).catch(err => console.error(err));
 
 
-
 })();
 
 
-function populateSfgMeta(sfg){
-    let sfgName=document.getElementById("sfg_meta_name");
+function populateSfgMeta(sfg) {
+    let sfgName = document.getElementById("sfg_meta_name");
     sfgName.innerHTML = sfg.name;
 
-    let sfgDate=document.getElementById("sfg_meta_date");
+    let sfgDate = document.getElementById("sfg_meta_date");
     sfgDate.innerHTML = sfg.measured_time;
 
     let maxIr = Math.max(sfg.ir);
-    let maxSfg= Math.max(sfg.sfg_intenstiy);
+    let maxSfg = Math.max(sfg.sfg_intenstiy);
     let maxVis = Math.max(sfg.vis);
-    document.getElementById("sfg_meta_ir").innerHTML=maxIr;
-    document.getElementById("sfg_meta_sfg").innerHTML=maxSfg;
-    document.getElementById("sfg_meta_vis").innerHTML=maxVis;
+    document.getElementById("sfg_meta_ir").innerHTML = maxIr;
+    document.getElementById("sfg_meta_sfg").innerHTML = maxSfg;
+    document.getElementById("sfg_meta_vis").innerHTML = maxVis;
 
 }
 
-function searchHandler(){
+function searchHandler() {
     console.log("lelele")
 }
 
 function plotSfg(sfg) {
-    TESTER = document.getElementById('tester');
-    Plotly.newPlot(TESTER, [{
-        x: sfg.wavenumber,
-        y: sfg.sfg_intensity,
-
-    }], {
-        title:sfg.name,
-        xaxis: {
+    document.getElementById("headline").innerHTML = sfg.name;
+    const layout = {
+        paper_bgcolor: 'rgb(180,180,199)',
+        plot_bgcolor: 'rgb(180,180,199)',
+        autosize: true,
+        //margin: {l: 0.1, t: 0.1, r: 0.1, b: 0.1},
+        yaxis: {
+            automargin: true,
             title: 'wavenumber / cm-1'
         },
-        yaxis: {
+        xaxis: {
+            automargin: true,
             title: 'norm SFG intensity/ arb.u.'
-        }
-    });
+        },
+
+    }
+
+    TESTER = document.getElementById('tester');
+    Plotly.newPlot(TESTER, [{
+            x: sfg.wavenumber,
+            y: sfg.sfg_intensity,
+
+        }], layout,
+        {responsive: true});
 }
 
+
+const config = {
+    responsive: true
+}
